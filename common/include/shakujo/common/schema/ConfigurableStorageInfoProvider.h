c34e6da59a391eaca41347beaa21f9ad59212691
Suguru ARAKAWA <arakawa@nautilus-technologies.com>
2019-03-05 20:03:43 +0900

    make ConfigurableStorageInfoProvider thread safe.

--- common/include/shakujo/common/schema/ConfigurableStorageInfoProvider.h
+++ common/include/shakujo/common/schema/ConfigurableStorageInfoProvider.h
@@ -17,11 +17,19 @@
 #ifndef SHAKUJO_COMMON_SCHEMA_CONFIGURABLE_STORAGE_INFO_PROVIDER_H_
 #define SHAKUJO_COMMON_SCHEMA_CONFIGURABLE_STORAGE_INFO_PROVIDER_H_
 
+#include <algorithm>
+#include <map>
+#include <mutex>
 #include <utility>
-#include <vector>
+#include <stdexcept>
+
+#include <cstring>
 
 #include "StorageInfoProvider.h"
 #include "TableInfo.h"
+#include "IndexInfo.h"
+
+#include "../util/utility.h"
 
 namespace shakujo::common::schema {
 
@@ -29,28 +37,96 @@ namespace shakujo::common::schema {
  * @brief configurable StorageInfoProvider.
  */
 class ConfigurableStorageInfoProvider : public StorageInfoProvider {
-private:
-    std::vector<TableInfo> entries_;
-
 public:
+    /**
+     * @brief constructs a new object.
+     * @param case_sensitive compare name with case sensitive
+     */
+    ConfigurableStorageInfoProvider(bool case_sensitive = true) noexcept  // NOLINT
+        : entries_(Comparator(case_sensitive))
+    {}
+
     /**
      * @brief adds an table.
      * @param table the target table information
+     * @param overwrite overwrite the existing table
      * @return this
+     * @throw std::invalid_argument on overwrite is disabled, if a table with same name already exists in this provider
      */
-    ConfigurableStorageInfoProvider& add(TableInfo table) {
-        entries_.push_back(std::move(table));
+    ConfigurableStorageInfoProvider& add(TableInfo table, bool overwrite = true) {
+        std::unique_lock lock { mutex_ };
+        if (auto it = entries_.find(table.name()); it != entries_.end()) {
+            if (overwrite) {
+                it->second = std::move(table);
+            } else {
+                throw std::invalid_argument(util::to_string("table \"", table.name(), "\" already exists"));
+            }
+        } else {
+            entries_.emplace(table.name(), std::move(table));
+        }
         return *this;
     }
 
     TableInfo const& find_table(core::Name const& name) const override {
-        for (auto& table : entries_) {
-            if (name == table.name()) {
-                return table;
-            }
+        std::unique_lock lock { mutex_ };
+        if (auto it = entries_.find(name); it != entries_.end()) {
+            return it->second;
         }
         return StorageInfoProvider::find_table(name);
     }
+
+private:
+    class Comparator {
+    public:
+        explicit constexpr Comparator(bool case_sensitive = true) noexcept : case_sensitive_(case_sensitive) {}
+        static constexpr char to_upper(char c) {
+            if ('a' <= c && c <= 'z') {
+                return static_cast<char>(c - 0x20);
+            }
+            return c;
+        }
+
+        inline bool operator()(core::Name const& a, core::Name const& b) const {
+            auto&& as = a.segments();
+            auto&& bs = b.segments();
+            for (std::size_t i = 0, n = std::min(as.size(), bs.size()); i < n; ++i) {
+                int cmp = compare(as[i], bs[i]);
+                if(cmp != 0) {
+                    return cmp < 0;
+                }
+            }
+            return as.size() < bs.size();
+        }
+
+        int compare(std::string_view a, std::string_view b) const {
+            if (case_sensitive_) {
+                return a.compare(b);
+            }
+            for (std::size_t i = 0, n = std::min(a.size(), b.size()); i < n; ++i) {
+                auto ac = to_upper(a[i]);
+                auto bc = to_upper(b[i]);
+                if (ac < bc) {
+                    return -1;
+                }
+                if (ac > bc) {
+                    return +1;
+                }
+            }
+            if (a.size() < b.size()) {
+                return -1;
+            }
+            if (a.size() > b.size()) {
+                return +1;
+            }
+            return 0;
+        }
+
+    private:
+        bool case_sensitive_;
+    };
+
+    std::map<common::core::Name, TableInfo, Comparator> entries_;
+    mutable std::mutex mutex_ {};
 };
 
 }  // namespace shakujo::common::schema
---
+++ common/test/schema/ConfigurableStorageInfoProviderTest.cpp
@@ -0,0 +1,132 @@
+/*
+ * Copyright 2018-2018 shakujo project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "shakujo/common/schema/ConfigurableStorageInfoProvider.h"
+
+#include <gtest/gtest.h>
+
+#include <functional>
+#include <future>
+#include <string>
+#include <memory>
+#include <thread>
+
+#include "shakujo/common/core/type/Int.h"
+
+namespace shakujo::common::schema {
+
+namespace t = core::type;
+
+class ConfigurableStorageInfoProviderTest : public ::testing::Test {};
+
+TEST_F(ConfigurableStorageInfoProviderTest, simple) {
+    ConfigurableStorageInfoProvider provider {};
+    provider.add({
+        "T",
+        {
+            { "C1", t::Int(32U) },
+        }
+    });
+    {
+        auto&& table = provider.find_table(common::core::Name("T"));
+        EXPECT_EQ(table.name(), "T");
+    }
+    {
+        auto&& table = provider.find_table(common::core::Name("t"));
+        EXPECT_FALSE(table.is_valid());
+    }
+}
+
+TEST_F(ConfigurableStorageInfoProviderTest, case_insensitive) {
+    ConfigurableStorageInfoProvider provider { false };
+    provider.add({
+        "T",
+        {
+            { "C1", t::Int(32U) },
+        }
+    });
+    {
+        auto&& table = provider.find_table(common::core::Name("T"));
+        EXPECT_EQ(table.name(), "T");
+    }
+    {
+        auto&& table = provider.find_table(common::core::Name("t"));
+        EXPECT_EQ(table.name(), "T");
+    }
+}
+
+TEST_F(ConfigurableStorageInfoProviderTest, overwrite) {
+    ConfigurableStorageInfoProvider provider {};
+    provider.add({
+        "T",
+        {
+            { "C1", t::Int(32U) },
+        }
+    });
+    provider.add({
+        "T",
+        {
+            { "C1", t::Int(32U) },
+            { "C2", t::Int(32U) },
+        }
+    });
+    try {
+        provider.add({
+            "T",
+            {
+                { "C1", t::Int(32U) },
+                { "C2", t::Int(32U) },
+                { "C3", t::Int(32U) },
+            }
+        }, false);
+        FAIL();
+    } catch (std::invalid_argument&) {
+        // ok
+    }
+    auto&& table = provider.find_table(common::core::Name("T"));
+    EXPECT_EQ(table.name(), "T");
+    EXPECT_EQ(table.columns().size(), 2);
+}
+
+
+TEST_F(ConfigurableStorageInfoProviderTest, concurrent) {
+    constexpr std::size_t count = 100;
+    ConfigurableStorageInfoProvider provider {};
+    std::function<void()> adder = [&]() {
+        for (std::size_t i = 0; i < count; ++i) {
+            provider.add({
+                std::to_string(i),
+                {
+                    { "C1", t::Int(32U) },
+                }
+            });
+        }
+    };
+    auto r1 = std::async(std::launch::async, adder);
+    auto r2 = std::async(std::launch::async, adder);
+    auto r3 = std::async(std::launch::async, adder);
+    r1.wait();
+    r2.wait();
+    r3.wait();
+    ASSERT_TRUE(r1.valid());
+    ASSERT_TRUE(r2.valid());
+    ASSERT_TRUE(r3.valid());
+    for (std::size_t i = 0; i < count; ++i) {
+        auto&& table = provider.find_table(common::core::Name(std::to_string(i)));
+        EXPECT_TRUE(table.is_valid());
+    }
+}
+
+}  // namespace shakujo::common::schema
